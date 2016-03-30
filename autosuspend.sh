981533bcaa18c5acb956e725850addfc8d71f5f1
MaximeMorel <maxime.morel69@gmail.com>
2016-03-30 17:30:09 +0100

    Automatic detection of pm or systemctl

--- autosuspend.sh
+++ autosuspend.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # autosuspend script :
-# After being launched, it will suspend the machine (using pm-suspend) after
-# 1h if there is no user active on a console and no network traffic (<100ko/s)
+# After being launched, it will hibernate the machine (using pm-hibernate or
+# systemctl hibernate) after 1h if there is no user active on a console and
+# no network traffic (<100kB/s)
 # Author : Maxime Morel <maxime.morel69@gmail.com>
 
 # Changelog
+# 02/10/2015 :
+# Shutdown if kernel has been updated, to prevent any stall during wakeup
+# Check for systemctl and use it if available instead of pm-hibernate
+#
 # 06/01/2014 : Actually quite ugly, but does what I need :
 # allow me to wake my storage machine and let it autosuspend if unused.
 #
@@ -14,7 +19,7 @@
 # fix getting stat info with ls -lu (atime field)
 
 # countdown in seconds
-countdown=3600
+countdown=14400
 
 # timestep in seconds
 timestep=30
@@ -25,6 +30,9 @@ idletime=60
 # network threshold to go below before sleep in kBps
 netthreshold=100
 
+# network interface to monitor
+network_interface=eth0
+
 # nb iterations (test purpose)
 nbiter=10
 
@@ -34,7 +42,7 @@ function isUserActive()
     n=$(date "+%s")
     for tty in $(w -h | tr -s ' ' | cut -d' ' -f2)
     do
-	# stat the tty device to have la modif time
+	# stat the tty device to have last modif time
 	lastmodif=$(ls -lu --time-style=full-iso /dev/$tty | tr -s ' ' | cut -d' ' -f 7,8,9)
         d=$(date -d "$lastmodif" "+%s")	# unix timestamp
         t=$(( $n - $d )) # idletime
@@ -49,7 +57,7 @@ function isUserActive()
 
 function getTotalTraffic()
 {
-        val=$(cat /proc/net/dev | grep eth0 | tr -s ' ' | cut -f3,11 -d ' ')
+        val=$(cat /proc/net/dev | grep $network_interface | tr -s ' ' | cut -f3,11 -d ' ')
         down=$(echo $val | cut -f1 -d' ') # down
         up=$(echo $val | cut -f2 -d' ') # up
         # sum
@@ -80,6 +88,33 @@ function isNetworkUsed()
         echo "network active"
         res=1
     fi
+}
+
+function sendHibernate()
+{
+	sync
+	if [ -d /lib/modules/$(uname -r) ]
+	then
+		which systemctl > /dev/null
+		if [ $? -eq 0 ]
+		then
+			#systemctl hibernate
+			systemctl poweroff
+		else
+		        #pm-suspend
+		        #pm-hibernate
+			shutdown -h now
+		fi
+	else
+		echo "The kernel has been updated, better to shutdown"
+		which systemctl > /dev/null
+		if [ $? -eq 0 ]
+		then
+			systemctl poweroff
+		else
+			shutdown -h now
+		fi
+	fi
 }
 
 start=$(date "+%s")
@@ -113,7 +148,7 @@ do
     then
         echo Go to suspend
         # suspend command
-        pm-suspend
+	sendHibernate
 
         # reset countdown
         mycountdown=$countdown
@@ -124,3 +159,4 @@ do
 
     mynbiter=$(( $mynbiter + 1 ))
 done
+
